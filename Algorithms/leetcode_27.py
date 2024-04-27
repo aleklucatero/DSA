dc46c6fef1a0368c48718e4c2b53f7bda4d2bbc7
aleklucatero <lucaalek99@gmail.com>
2024-04-26 21:07:14 -0700

    Solved Leetcode 27
    Used pointers to solve this problem because I am altering the array while iterating over it. I used the pop method to pop whatever indices the pointer was at if it equaled the val provided by the user.

--- Algorithms/leetcode_27.py
+++ Algorithms/leetcode_27.py
@@ -6,11 +6,12 @@ number of elements in nums which are not equal to val.
 1. Conceptual understanding:
 Given a list of nums, Iterate through the list looking for a specific value to 
 delete. Keep the values in the same array, just delete the nums that equal
-the value. Print out the length of the final array, as well as the array
+the value. Print out the length of the final array, as well as the array. 
+Since we are working with the same list and changing it, use a pointer.
 
 2. Conceptual Solution:
-    Example 1: nums = [3,2,2,3], val = 3 => 2, nums = [2,2,_,_]
-    Example 2: nums = [0,1,2,2,3,0,4,2], val = 2 => 5, nums = [0,1,4,0,3,_,_,_]
+    Example 1: nums = [3,2,2,3], val = 3 => 2
+    Example 2: nums = [0,1,2,2,3,0,4,2], val = 2 => 5
 
 3. Pseudocode:
     a. Define function that takes nums, var as parameter.
@@ -24,13 +25,38 @@ the value. Print out the length of the final array, as well as the array
 '''
 
 #Code Attempt 1:
-def removeElement(nums, val):
+# def removeElement(nums, val):
 
-    for num in nums:
-        if num == val:
-            nums.remove(num)
+#     for num in nums:
+#         if num == val:
+#             nums.remove(num)
+#         else:
+#             continue
+#     return len(nums), "nums =", nums
+
+# print(removeElement([3,2,2,3], 3))
+
+'''
+3. Pseudocode:
+    a. Define function that takes nums, var as parameter.
+        b. initialize pointer
+        c. while len(nums) > pointer
+            d. if the current index equal val
+                e. nums.pop(pointer)
+            f. else 
+                g. increase pointer index
+        h. return length of array
+'''
+
+#Code Attempt 2:
+def removeElements(nums, val):
+    i = 0
+    while i < len(nums):
+        if nums[i] == val:
+            nums.pop(i)
         else:
-            continue
-    return len(nums), "nums =", nums
+            i += 1
+    return len(nums)
 
-print(removeElement([3,2,2,3], 3))
+print(removeElements([3,2,2,3], 3))
+print(removeElements([0,1,2,2,3,0,4,2], 2))
